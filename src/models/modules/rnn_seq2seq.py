38e9f99e8a2bf817233490f0838d84831006bcf4
jopetty <research@jacksonpetty.org>
2022-08-31 16:23:11 -0400

    Pass in tf_ratio, formatting

--- src/models/modules/rnn_seq2seq.py
+++ src/models/modules/rnn_seq2seq.py
@@ -13,9 +13,9 @@ class RNNSeq2Seq(nn.Module):
         self.encoder = RNNEncoder(hparams)
         self.decoder = RNNDecoder(hparams)
 
-    def forward(self, enc_input: Dict[str, Tensor]):
+    def forward(self, enc_input: Dict[str, Tensor], tf_ratio: float):
 
         dec_input = self.encoder(enc_input) | enc_input
-        output = self.decoder(dec_input, tf_ratio=1.0)
+        output = self.decoder(dec_input, tf_ratio=tf_ratio)
 
         return output
